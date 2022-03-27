88acca58523a53989106b6e6db1db8c37905207f
Mechack Ira <mechackniyomukiza@gmail.com>
2022-03-26 21:24:10 -0400

    database and get api

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {Row,Col,Image,ListGroup,Button,Card} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 function ProductScreen({match}) {
-    const product = products.find((p) => p._id === match.params.id)
+    const [product,setProducts] = useState([])
+
+    useEffect(() => {
+      async function fetchProduct(){
+        const { data} = await axios.get(`/api/products/${match.params.id}`)
+        setProducts(data)
+      }
+      fetchProduct()
+    }, [])
   return (
     <div>
         <Link to='/' classname='btn btn-light my-3'>Go Back</Link>
